41d4013525f5d726a4e9208f1a94500f2413bdac
Noor Muhammad Light <77665556+nooralight@users.noreply.github.com>
2023-03-06 18:04:41 +0600

    Update water_mark.py

--- water_mark.py
+++ water_mark.py
@@ -9,27 +9,23 @@ class Water_Mark:
     def get_result(self):
         
         response = requests.get(self.url)
-        img = Image.open(BytesIO(response.content)) 
+        img = Image.open(BytesIO(response.content))
         #Creating draw object
-        draw = ImageDraw.Draw(img) 
+        draw = ImageDraw.Draw(img)
 
         #Creating text and font object
-        text = "GenAiToken.com"
+        text = "muratiAI.com Beta"
         font = ImageFont.truetype('GothamBold.ttf', 15)
-        width, height = img.size
-        text_width, text_height = draw.textsize(text, font)
-
-
+        text_size = draw.textsize(text,font=font)
         #Positioning Text
-        
-        x=8
-        y=8
-        # Add a shadow border to the text
+        x=6
+        y=6
         for offset in range(1, 2):
-            draw.text((x - offset, y-offset), text, font=font, fill=(88, 88, 88))
-            
+            draw.text((x - offset, y), text, font=font, fill=(90,90,90))
+            draw.text((x + offset, y), text, font=font, fill=(90,90,90))
+            draw.text((x, y + offset), text, font=font, fill=(90,90,90))
+            draw.text((x, y - offset), text, font=font, fill=(90,90,90))
         #Applying text on image via draw object
-        draw.text((x, y), text, font=font,fill=(255,255,255)) 
-
+        draw.text((x, y), text, font=font,fill=(255, 255, 255))
         #Saving the new image
         img.save("result.png")
